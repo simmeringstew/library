9d01612866c232c2f5bcffacb1c6088682b6c372
simmeringstew <dev.95a2e@simplelogin.com>
2022-10-19 10:54:57 -0400

    Added form validation to ensure that they are not empty

--- script.js
+++ script.js
@@ -12,3 +12,55 @@ const cancelButton = document.querySelector(".cancel");
 cancelButton.addEventListener("click", () => {
     popup.classList.remove("show");
 });
+
+// allows the form to be submited either through the button or pressing enter
+
+const submit = document.querySelector(".submit");
+const form = document.querySelector(".form");
+
+submit.addEventListener("click", validateForm);
+
+form.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) {
+        validateForm();
+    }
+    else {
+        return;
+    }
+});
+
+// function to validate whether the user has entered information into the form
+
+function validateForm() {
+    let emptyCheck = 0;
+    const title = document.querySelector("#title");
+    const titleError = document.querySelector(".title-error-message");
+    const author = document.querySelector("#author");
+    const authorError = document.querySelector(".author-error-message");
+    const pages = document.querySelector("#pages");
+    const pagesError = document.querySelector(".pages-error-message");
+    const read = document.querySelector("#read");
+
+    if (title.value.trim() === "") {
+        title.classList.add("error");
+        titleError.classList.add("error");
+        emptyCheck++;
+    }
+    if (author.value.trim() === "") {
+        author.classList.add("error");
+        authorError.classList.add("error");
+        emptyCheck++
+    }
+    if (pages.value.trim() === "") {
+        pages.classList.add("error");
+        pagesError.classList.add("error");
+        emptyCheck++;
+    }
+
+    if (emptyCheck > 0) {
+        return;
+    }
+    else {
+        console.log(read.checked);
+    }
+}
